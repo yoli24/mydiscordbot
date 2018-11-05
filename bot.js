1de40d29453a426716f6a55a9caa8f0778d8ec11
yoli24 <43812870+yoli24@users.noreply.github.com>
2018-11-05 11:27:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,15 +3,12 @@ const bot = new Discord.Client();
 const fs = require('fs');
 
 bot.on('ready', async()=>{
-    console.log(fs.readFile('./file.json'));
     var x = {
         "test": "1"
     };
     fs.writeFileSync('./file.json', JSON.stringify(x));
     //console.log("\x1b[42m%s\x1b[0m", `Connected to ${bot.user.tag}!`);
-    console.log(fs.readFile('./file.json'));
 
 });
-
 
 bot.login(process.env.BOT_TOKEN);
