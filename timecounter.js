1b81756d795c6b372cc106b086d263ed12dc43b7
yoli24 <43812870+yoli24@users.noreply.github.com>
2018-11-08 10:10:57 +0200

    Update timecounter.js

--- timecounter.js
+++ timecounter.js
@@ -22,6 +22,7 @@ class TimeCounter{
             user = bot.users.find(user=> user.id == notifications[i]);
             this.TodayToString(bot, user);
         }
+        CheckDate(bot);
     }
     static NewDate(){
         currentDate = new Date();
